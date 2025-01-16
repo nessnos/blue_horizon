1445c82609cc69215c1e8a212b53e799de92d139
Ines Boulahya <nesssnos@gmail.com>
2025-01-16 23:14:15 +0100

    Reset pages as dashboard style - ui map page

--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -16,16 +16,29 @@
       <div class="flex flex-col gap-2 items-start justify-between border-[0.5px] border-gray-300 rounded-lg bg-gray-100/55 shadow px-6 py-4 h-fit min-h-64 min-w-56">
         <div class="relative w-56 h-full p-1 py-4">
           <img class="rounded" src='@/assets/img/statistics.png' alt="">
-          <div class="absolute bottom-0 w-full h-14 blur-lg bg-gray-100"></div><!-- Blur effect -->
+          <div class="absolute bottom-0 w-full h-12 blur-lg bg-gray-100"></div><!-- Blur effect -->
         </div>
-        <div class="py-3">
-          <div class="text-sm font-normal">Stastical Anaylsis</div>
-          <div class="text-xs py-2">jsjqdkjksdef</div>
+        <div class="py-3 max-w-56">
+          <div class="text-sm font-normal">Statistics Dashboard</div>
+          <div class="text-xs py-2">
+            Access a dashboard with water quality metrics like pH, pollution, and availability. 
+            Filter by country or year to track trends and explore changes in water health over time.
+          </div>
         </div>
         <div class="bg-ocean rounded-lg px-3 py-2 text-white text-sm hover:bg-aqua hover:cursor-pointer">Learn More</div>
       </div>
-      <div class="flex flex-col gap-2 items-start justify-between border-[0.5px] border-gray-400 rounded-lg bg-white shadow px-6 py-4 h-64 min-w-56">
-        <div class="text-sm">Interactive Map</div>
+      <div class="flex flex-col gap-2 items-start justify-between border-[0.5px] border-gray-300 rounded-lg bg-gray-100/55 shadow px-6 py-4 h-fit min-h-64 min-w-56">
+        <div class="relative w-56 h-full p-1 py-4">
+          <img class="rounded" src='@/assets/img/map.png' alt="">
+          <div class="absolute bottom-0 w-full h-12 blur-lg bg-gray-100"></div><!-- Blur effect -->
+        </div>
+        <div class="py-3 max-w-56">
+          <div class="text-sm font-normal">Interactive Map</div>
+          <div class="text-xs py-2">
+            Explore Europe’s water quality through an interactive map. 
+            Select countries to view detailed metrics, offering localized insights into water health and key statistics.
+          </div>
+        </div>
         <div class="bg-ocean rounded-lg px-3 py-2 text-white text-sm hover:bg-aqua hover:cursor-pointer">Learn More</div>
       </div>
     </div>
--- webapp/src/views/Map.vue
+++ webapp/src/views/Map.vue
@@ -4,8 +4,15 @@ import Europe from "./Europe.vue"
 </script>
 <template>
 
- <div>
-    <Europe class="fill-gray-300 h-[57.3rem] w-fit"/>
+ <div class="flex flex-col gap-3">
+   <div class="p-3 min-h-24">
+      Filters
+   </div>
+   <div class="flex flex-row">
+      <Europe class="h-full w-3/4"/>
+      <div>Panel</div>
+   </div>
+    
  </div>
 
 </template>
