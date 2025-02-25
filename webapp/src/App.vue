01a00bde560f17280261fe71684bd2a92f7c97c5
Ines Boulahya <ines.boulahya@ulb.be>
2025-02-25 12:38:19 +0000

    important changes in packages and respecting name conventions

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,35 +1,8 @@
-<script lang="ts" setup>
-import { ref } from "vue"
-import { RouterLink, RouterView, useRoute } from "vue-router"
-import { ChartPieIcon, HomeIcon, MapIcon } from "@heroicons/vue/24/outline"
-
-const isOpen = ref(false)
-
-const openSidebar = () => {
-  console.log(isOpen.value)
-  return (isOpen.value = !isOpen.value)
-}
-
-const isActive = (path: string) => {
-  const route = useRoute()
-  return route.path === path
-}
-</script>
 <template>
   <div class="flex flex-row">
     <div
-      :class="[[isOpen ? 'w-72' : '']]"
-      class="absolute inline-flex h-screen transform flex-col items-center bg-ocean p-2 px-3 shadow transition transition-all duration-200 duration-300 ease-in-out md:relative md:left-0"
+      class="absolute inline-flex h-screen transform flex-col items-center bg-ocean p-2 px-3 shadow transition-all duration-300 ease-in-out md:relative md:left-0"
     >
-      <div class="my-6 flex w-full items-end justify-between px-4">
-        <div v-if="isOpen" class="text-xl font-bold text-white">
-          Blue Horizon
-        </div>
-        <!--≈<div @click="openSidebar"  class="p-2 rounded-full text-center text-white bg-gray-500/55">
-          <ChevronLeftIcon v-if="isOpen" class="h-4 w-4" />
-          <ChevronRightIcon v-if="!isOpen" class="h-4 w-4" />
-        </div>-->
-      </div>
       <nav
         class="mt-6 inline-flex flex-col space-y-2"
         v-bind:class="{ 'w-full': isOpen }"
@@ -65,3 +38,13 @@ const isActive = (path: string) => {
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { RouterLink, RouterView, useRoute } from "vue-router"
+import { ChartPieIcon, HomeIcon, MapIcon } from "@heroicons/vue/24/outline"
+
+const isActive = (path: string) => {
+  const route = useRoute()
+  return route.path === path
+}
+</script>
